1c30952738dd0f947178b5831a95a2fa4f15995f
Sparkmod <rafadagher@gmail.com>
2021-09-21 07:50:06 -0400

    fix: ItemService from Database
    # TODO
    - Refactor Parser data to db
    - Refactor how Services and Manager gets the items.

--- Maplecodex2/Data/Services/ItemService.cs
+++ Maplecodex2/Data/Services/ItemService.cs
@@ -1,10 +1,17 @@
 ﻿using Maplecodex2.Data.Storage;
 using Maplecodex2.Data.Models;
+using Maplecodex2.Database.Managers;
 
 namespace Maplecodex2.Data.Services
 {
     public class ItemService
     {
-        public Task<Item> GetItemAsync(int id) => Task.FromResult(ItemStorage.GetItem(id));
+        public Task<Item> GetItemAsync(int id)
+        {
+            ItemManager manager = new ItemManager(Settings.GetDbContext());
+
+            return manager.Get(id);
+            //return Task.FromResult(ItemStorage.GetItem(id));
+        }
     }
 }
