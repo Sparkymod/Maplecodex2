367073311c84cc966b228a3d15419c5fa58fd218
Sparkmod <rafadagher@gmail.com>
2021-09-23 19:25:55 -0400

    Fix: master

--- Maplecodex2.DBSync/Program.cs
+++ Maplecodex2.DBSync/Program.cs
@@ -2,19 +2,18 @@
 using Maplecodex2.Data.Services;
 using Maplecodex2.DBSync.Data.Storage;
 using Serilog;
-using System;
 using System.Diagnostics;
 
 namespace Maplecodex2.DBSync
 {
     public class Program
     {
-        public static async void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             Settings.InitializeSerilog();
             Settings.InitDatabase();
 
-            Log.Logger.Warning($"ARE YOU WANT TO PARSE THE DATABASE? \"Yes\" to parse.");
+            Log.Logger.Warning($"ARE YOU WANT TO PARSE THE DATABASE? \"yes\" to parse.");
 
             string? read = Console.ReadLine();
             if (read != null && read.Contains("yes", StringComparison.OrdinalIgnoreCase))
