4acc780179cdac62c0a556c8b4a2f1749024e11e
Sparkmod <rafadagher@gmail.com>
2021-10-05 13:15:19 -0400

    Feat: Copy ID to clipboard
    ## Added
    - Copy ID to clipboard when clicked.
    
    ## Fixes
    - Fix an issue when searching and the pagination doesn't reset.

--- Maplecodex2/wwwroot/js/Helper.js
+++ Maplecodex2/wwwroot/js/Helper.js
@@ -1,4 +1,31 @@
-﻿function CopyToClipboard() {
-    /* TODO */
-    var itemIds = document.querySelectorAll('#itemId');
+﻿// Initializer
+$(document).ready(CopyToClipboard());
+
+function CopyToClipboard() {
+    document.addEventListener('click', (e) => {
+        // Message to show when copy.
+        const message = "Copied!"
+
+        // Event argument to be either e or window.event.
+        e = e || window.event;
+
+        // Store the click element.
+        const target = e.target || e.srcElement;
+
+        // We just want the table data clicked so we search by id.
+        if (target.id === "itemTableId" && target.textContent != message) {
+            text = target.textContent || target.innerText;
+
+            // Copy the text to the clipboard.
+            navigator.clipboard.writeText(text);
+
+            // Add style when click for 3 seconds, then remove.
+            target.classList.toggle("copied");
+            target.textContent = message;
+            setTimeout(function () {
+                target.classList.toggle("copied");
+                target.textContent = text;
+            }, 2000);
+        }
+    }, false);
 }
