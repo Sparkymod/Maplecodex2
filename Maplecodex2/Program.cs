6d3442f57072b8970fbecb53c7059d837449068a
Sparkmod <rafadagher@gmail.com>
2021-10-06 07:40:14 -0400

    Fix: Redirect to HTTPS

--- Maplecodex2/Program.cs
+++ Maplecodex2/Program.cs
@@ -2,6 +2,7 @@ using Maplecodex2;
 using Maplecodex2.Data.Extensions;
 using Maplecodex2.Data.Services;
 using Serilog;
+using Microsoft.AspNetCore.Rewrite;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -9,19 +10,21 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<ItemService>();
-//builder.WebHost.UseUrls("http://*:5000");
+
 
 // Set serilog configuration.
 builder.Host.UseSerilog(Settings.InitializeSerilog());
 
 WebApplication app = builder.Build();
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseSerilogRequestLogging();
 app.UseRouting();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+//app.UseRewriter(new RewriteOptions().AddApacheModRewrite(builder.Environment.ContentRootFileProvider, ".htaccess"));
+app.UseRewriter(new RewriteOptions().AddRedirectToHttps());
 
 // Initialization
 Settings.InitDatabase();
