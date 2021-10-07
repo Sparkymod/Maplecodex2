fbe81971d1d4cd5823baf1837f63a85345c4a120
Sparkmod <rafadagher@gmail.com>
2021-10-07 09:49:14 -0400

    hotfix: Add redirection to SSL

--- Maplecodex2/Program.cs
+++ Maplecodex2/Program.cs
@@ -2,7 +2,7 @@ using Maplecodex2;
 using Maplecodex2.Data.Extensions;
 using Maplecodex2.Data.Services;
 using Serilog;
-using Microsoft.AspNetCore.Rewrite;
+using Microsoft.AspNetCore.Authentication.Certificate;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +10,8 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<ItemService>();
-
+builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme).AddCertificate().AddCertificateCache();
+builder.WebHost.UseUrls("https://*:5001");
 
 // Set serilog configuration.
 builder.Host.UseSerilog(Settings.InitializeSerilog());
@@ -18,13 +19,12 @@ builder.Host.UseSerilog(Settings.InitializeSerilog());
 WebApplication app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseStaticFiles();
 app.UseSerilogRequestLogging();
 app.UseRouting();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-//app.UseRewriter(new RewriteOptions().AddApacheModRewrite(builder.Environment.ContentRootFileProvider, ".htaccess"));
-app.UseRewriter(new RewriteOptions().AddRedirectToHttps());
 
 // Initialization
 Settings.InitDatabase();
