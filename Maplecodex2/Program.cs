0cb37ea30b3552795417e65c70526593292bca7e
Sparkmod <rafadagher@gmail.com>
2021-10-07 15:41:39 -0400

    Feat: Base created for item preview

--- Maplecodex2/Program.cs
+++ Maplecodex2/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<ItemService>();
 builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme).AddCertificate().AddCertificateCache();
-builder.WebHost.UseUrls("https://*:5001");
+//builder.WebHost.UseUrls("https://*:5001");
 
 // Set serilog configuration.
 builder.Host.UseSerilog(Settings.InitializeSerilog());
